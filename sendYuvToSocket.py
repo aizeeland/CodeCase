b9c59ff715afefab4ddd14a7047a1676839efe00
zeeland ai <77146959+aizeeland@users.noreply.github.com>
2024-03-14 14:44:38 +0100

    Update sendYuvToSocket.py

--- sendYuvToSocket.py
+++ sendYuvToSocket.py
@@ -55,7 +55,7 @@ while True:
         os.remove(os.path.join(image_dir, filename))
 
     # Wait for a second before checking the directory again
-    time.sleep(1)
+    # time.sleep(1)
 
 # Close the sockets
 client_socket.close()
